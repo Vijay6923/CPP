ec9a66e34c8137967de1f22e1bcc6fd1194737fa
Vijay6923 <kumarviju2468@gmail.com>
2024-05-05 23:04:10 +0530

    revision

--- revision/count.cpp
+++ revision/count.cpp
@@ -1,15 +1,31 @@
-# include<iostream>
+// # include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=0;
+//     while(n>0){
+//         count++;
+//         n=n/10;
+
+
+//     }
+// cout<<count<<endl;
+// return 0;
+// }
+#include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    int count=0;
+    int power=1;
+    int ans=0;
     while(n>0){
-        count++;
+        int ld=n%10;
+        ans+=ld*power;
+        power*=2;
         n=n/10;
-
-
     }
-cout<<count<<endl;
-return 0;
+    cout<<ans;
+    return 0;
 }
