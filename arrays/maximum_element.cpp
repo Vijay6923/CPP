a18ab77faa06b42ed3e379f9c01595f2f0dad95f
Vijay6923 <kumarviju2468@gmail.com>
2024-07-04 23:08:33 +0530

    update

--- arrays/maximum_element.cpp
+++ arrays/maximum_element.cpp
@@ -1,11 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int n;
-    // int arr[n];
-    // for (int i=0;i<n;i++){
-    //     cin>>arr[i];
-    // }
+   
     int arr[]={3,5,7,9,17,543};
     int max=arr[0];
     for (int i=1;i<6;i++){
