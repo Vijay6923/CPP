dc0de2a6123cda4c7366d340cc61c18f0501e784
Vijay6923 <kumarviju2468@gmail.com>
2024-07-05 23:22:18 +0530

    codeforces

--- arrays/sumOfTheelement.cpp
+++ arrays/sumOfTheelement.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int n;
-    // int array[n];
-    // for(int i=0;1<5;i++){
-    //     cin>>array[i];
-
-    // }
+    /
     int array[]={1,4,2,3,5};
     
     int sum=0;
