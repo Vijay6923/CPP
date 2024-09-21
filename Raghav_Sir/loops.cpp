97b4f834ffb61cbbb27d918ab125f4b263506374
Vijay6923 <kumarviju2468@gmail.com>
2024-09-21 13:23:19 +0530

    revision

---
+++ Raghav_Sir/loops.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int sum=0;
+
+    for(int i=0;i<=9;i++){
+        sum +=i;
+        cout<<sum<<endl;
+
+    }
+    cout<<sum<<endl;
+    return 0;
+}
--- arrays/sumOfTheelement.cpp
+++ arrays/sumOfTheelement.cpp
@@ -1,14 +1,54 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+    
+//     int array[]={1,4,2,3,5};
+    
+//     int sum=0;
+//     for (int i=0;i<5;i++){
+//         sum+=array[i];
+//     }
+//     cout<<sum<<endl;
+//     return 0;
+
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     long long int a, b, c,d;
+//     cin >> a >> b >> c >> d;
+//     string ans="NO";
+//     if(a+b-c==d){
+//         ans="YES";
+//     }
+//     if(a+b*c==d){
+//         ans="YES";
+//     }
+//     if(a-b+c==d){
+//         ans="YES";
+//     }
+//     if(a-b*c==d){
+//         ans="YES";
+//     }
+//     if(a*b-c==d){
+//         ans="YES";
+//     }
+//     if(a*b+c==d){
+//         ans="YES";
+//     }
+
+//     cout<<ans<<endl;
+
+//     return 0;
+    
+
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    /
-    int array[]={1,4,2,3,5};
-    
-    int sum=0;
-    for (int i=0;i<5;i++){
-        sum+=array[i];
-    }
-    cout<<sum<<endl;
+    int n;
+    cin>>n;
+    cout<< n <<endl;
     return 0;
-
 }
