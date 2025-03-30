63ce728ffd89b57bb15c38e4323957e1c40c58ff
Vijay6923 <kumarviju2468@gmail.com>
2025-03-30 23:24:37 +0530

    revision stack

--- stack/basis.cpp
+++ stack/basis.cpp
@@ -3,5 +3,14 @@ using namespace std;
 int main() {
     stack<int>s;
     s.push(1);
-    cout<<s.top();
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    while(!s.empty()) {
+        cout << s.top() << endl;
+        s.pop(); 
+    }
+    
+    
 }
