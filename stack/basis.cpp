a0f65219a5ab09d9407bf09a26a8f776d8a8c4c4
Vijay6923 <kumarviju2468@gmail.com>
2025-03-31 20:38:57 +0530

    s

--- stack/basis.cpp
+++ stack/basis.cpp
@@ -11,6 +11,16 @@ int main() {
         cout << s.top() << endl;
         s.pop(); 
     }
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    while(!s.empty()) {
+        cout << s.top() << endl;
+        s.pop(); 
+    }
+
     
     
 }
